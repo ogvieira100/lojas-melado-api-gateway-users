5a81ad83eb12420a337d868ec6c663c71cc50bcd
ogvieira <osmargv100@gmail.com>
2024-08-11 09:09:10 -0300

    users

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
+using ApiGatewayUser.Model;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+IWebHostEnvironment environment = builder.Environment;
+
+builder.Configuration.AddJsonFile("appsettings.json", true, true)
+                    .SetBasePath(environment.ContentRootPath)
+                    .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+var appSettings = configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettings);
+var appSettingsValues = appSettings.Get<AppSettings>();
+
+/*cors*/
+builder.Services.AddCors(options =>
+{
+
+    options.AddPolicy("Development",
+          builder =>
+              builder
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowAnyOrigin()
+              ); // allow credentials
+
+    options.AddPolicy("Production",
+        builder =>
+            builder
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowAnyOrigin()
+              ); // allow credentials
+});
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
